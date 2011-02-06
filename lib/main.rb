964ae378dd12ca4e295347e6264d9d3f42a8e089
miyohide <miyohide@gmail.com>
2011-02-06 20:23:44 +0900

    bug fix. Not take into account the pattern of Double and Turkey.

--- lib/main.rb
+++ lib/main.rb
@@ -2,32 +2,29 @@ class Bowling
    def Bowling.score(pins)
 ##      pins.inject(0) {|result, item| result + item}
       result = 0
-      before_strike = false
-      before_spare = false
       pins_size = pins.size
       pins.each_with_index { |item,idx|
-         if idx == pins_size - 1 then
-            result += pins[idx]
-            break
-         end
-         if idx >= 2 && idx % 2 == 0 then
-            if pins[idx - 2] == 10 then
-               before_strike = true
-            elsif pins[idx - 2] + pins[idx - 1] == 10 then
-               before_spare = true
+         if idx < 16 then
+            if idx % 2 == 0 && pins[idx] == 10 then
+               result += pins[idx + 2]
+               if pins[idx + 2] == 10 then
+                  result += pins[idx + 4]
+               else
+                  result += pins[idx + 3]
+               end
+            elsif idx % 2 == 0 && pins[idx] + pins[idx + 1] == 10 then
+               result += pins[idx + 2]
             end
-         end
-         if before_strike then
-            result += pins[idx]
-         end
-         if before_spare && idx % 2 == 0 then
-            result += pins[idx]
+         elsif idx < 18 then
+            if idx % 2 == 0 && pins[idx] == 10 then
+               result += pins[idx + 2] + pins[idx + 3]
+            elsif idx % 2 == 0 && pins[idx] + pins[idx + 1] == 10 then
+               result += pins[idx + 2]
+            end
+         else
+
          end
          result += pins[idx]
-         if idx % 2 == 1 then
-            before_strike = before_spare = false
-         end
-
       }
       return result
    end
